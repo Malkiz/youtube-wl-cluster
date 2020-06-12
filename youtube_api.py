8495453ba2f2528db3a65b92c439a71da189f4aa
malkiz <omieyal@gmail.com>
2020-06-12 20:12:20 +0300

    another try to use oauth

--- youtube_api.py
+++ youtube_api.py
@@ -1,14 +1,40 @@
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
+# -*- coding: utf-8 -*-
 
-flow = InstalledAppFlow.from_client_secrets_file(
-    'client_secret.json',
-    scopes=['profile', 'email'])
+# Sample Python code for youtube.channels.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/guides/code_samples#python
 
-flow.run_local_server()
-credentials = flow.credentials
+import os
 
-youtube = build('youtube', 'v3', credentials=credentials)
-request = youtube.channels().list(part='statistics', forUsername='sentdex')
-response = request.execute()
-print(response)
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+
+scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+def main():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "client_secret.json"
+
+    # Get credentials and create an API client
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+    credentials = flow.run_console()
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+
+    request = youtube.channels().list(
+        part="snippet,contentDetails,statistics",
+        mine=True
+    )
+    response = request.execute()
+
+    print(response)
+
+if __name__ == "__main__":
+    main()
