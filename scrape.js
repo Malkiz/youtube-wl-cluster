f98d87ac8985ca1a127c28796ac12525abc62ff1
malkiz <omieyal@gmail.com>
2020-06-13 14:04:04 +0300

    try to sign in

--- scrape.js
+++ scrape.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const rimraf = require('rmfr');
+const account = require('./account');
 
 // https://github.com/puppeteer/puppeteer/issues/1837#issuecomment-522850970
 const PATHS = {
@@ -20,13 +21,31 @@ const options = {
   userDataDir: PATHS.win32.userDataDir
 };
 
+const elementSelectors = {
+  signIn: '#buttons > ytd-button-renderer > a',
+  emailInput: '#identifierId',
+  next: '#identifierNext',
+};
+
+const click = async (page, selector) => {
+  await page.waitForSelector(selector, {visible: true});
+  await page.click(selector);
+};
+const type = async (page, selector, text) => {
+  await page.waitForSelector(selector, {visible: true});
+  await page.type(selector, text);
+};
+
 (async () => {
   const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
-  await page.goto('https://google.com');
-  await page.screenshot({path: 'example.png'});
+  await page.goto('https://youtube.com');
+  
+  await click(page, elementSelectors.signIn);
+  await type(page, elementSelectors.emailInput, account.email);
+  await click(page, elementSelectors.next);
 
-  await browser.close();
+  //await browser.close();
 })()
 .catch( e => { console.error(e) })
 .finally(() => rimraf(PATHS[process.platform].userDataDir));
