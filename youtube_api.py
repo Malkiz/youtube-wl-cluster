554e5af6dba8336f67f8348cca4fd89e83f5c990
malkiz <omieyal@gmail.com>
2020-06-13 18:53:24 +0300

    get data by chunks

--- youtube_api.py
+++ youtube_api.py
@@ -1,5 +1,9 @@
 import json
 from googleapiclient.discovery import build
+import pandas as pd
+
+def chunk_df(df, n=10):
+    return [df[i:i+n] for i in range(0,df.shape[0],n)]
 
 def get_api_key():
     with open('apiKey.json') as json_file:
@@ -7,11 +11,16 @@ def get_api_key():
         api_key = data['key']
     return api_key
 
+wl = pd.read_csv('WL.csv')
+wl_chunks = chunk_df(wl, 50)
+
+#print(wl_chunks[0])
+
 api_key = get_api_key()
 youtube = build('youtube', 'v3', developerKey=api_key)
 request = youtube.videos().list(
     part="snippet,contentDetails,statistics",
-    id="Ks-_Mh1QhMc"
+    id=','.join(wl_chunks[0]['id'])
 )
 response = request.execute()
 
