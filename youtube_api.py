bf79a70ebff72de2aef6438b20d273d1169cf3b5
malkiz <omieyal@gmail.com>
2020-07-01 22:39:06 +0300

    start implementing stages system

--- youtube_api.py
+++ youtube_api.py
@@ -16,6 +16,7 @@ import numpy as np
 from sklearn import preprocessing
 from matplotlib import cm
 from sklearn.decomposition import FactorAnalysis
+import itertools
 
 def chunk_df(df, n=10):
     return [df[i:i+n] for i in range(0,df.shape[0],n)]
@@ -95,7 +96,7 @@ def get_videos_df():
     videos_df = videos_df.join(channels_df.set_index('id'), on='channelId', rsuffix='_channel')
     return videos_df.set_index('id')
 
-def get_features_df(videos_df):
+def get_features_df(videos_df, data_sets):
     # TODO:
     #   - mean & normalize numeric columns
     #       NOTE: K-means normalized the data automatically. But maybe it's needed for other models.
@@ -110,44 +111,52 @@ def get_features_df(videos_df):
     array_columns = ['tags','relevantTopicIds','topicCategories','topicIds']
     category_columns = ['channelId','categoryId']
 
-    all_dfs = []
-
-    if (args.text):
+    def text():
         print('using text data')
         vectorizer = CountVectorizer()
         corpus = videos_df.loc[:, text_columns].values.sum(axis=1)
         text = vectorizer.fit_transform(corpus).toarray()
         text_df = pd.DataFrame(text).set_index(videos_df.index)
         print('> added {} columns'.format(len(text_df.columns)))
-        all_dfs.append(text_df)
+        return text_df
 
-    if (args.categorical == 1):
+    def categorical_1():
         print('using categorical data - Gower')
         categorical_df = pd.DataFrame(gower.gower_matrix(videos_df.loc[:, category_columns], cat_features = [True for v in category_columns])).set_index(videos_df.index)
         print('> added {} columns'.format(len(categorical_df.columns)))
-        all_dfs.append(categorical_df)
+        return categorical_df
 
-    if (args.numerical):
+    def numerical():
         print('using numerical data')
         numerical_df = pd.DataFrame(preprocessing.normalize(videos_df.loc[:, numeric_columns].fillna(0))).set_index(videos_df.index)
-        all_dfs.append(numerical_df)
+        return numerical_df
 
     def get_array_dummies(df, column):
         return pd.get_dummies(df[column].fillna('').apply(pd.Series).stack(), dtype=int).sum(level=0)
 
-    if (args.array):
+    def array():
         print('using array data')
         dummies_arr = map(lambda col: get_array_dummies(videos_df, col), array_columns)
         dummies_df1 = pd.concat(dummies_arr, axis=1, sort=False)
         print('> added {} columns'.format(len(dummies_df1.columns)))
-        all_dfs.append(dummies_df1)
+        return dummies_df1
 
-    if (args.categorical == 2):
+    def categorical_2():
         print('using categorical data - dummies')
         dummies_df2 = pd.concat(map(lambda col: pd.get_dummies(videos_df[col].fillna(''), dtype=int), category_columns), axis=1, sort=False)
         print('> added {} columns'.format(len(dummies_df2.columns)))
-        all_dfs.append(dummies_df2)
+        return dummies_df2
        
+    data_getters = {
+        'text':text,
+        'categorical_1':categorical_1,
+        'numerical':numerical,
+        'array':array,
+        'categorical_2':categorical_2
+    }
+    all_dfs = [data_getters[n]() for n in data_sets]
+    return all_dfs
+
     features_df = pd.concat(all_dfs, axis=1, sort=False)
     print('features is {} dimentions'.format(len(features_df.columns)))
 
@@ -271,7 +280,9 @@ def visualize(results, videos_df, features_df):
 
 def main():
     videos_df = get_videos_df()
-    features_df = get_features_df(videos_df)
+
+    unique_data = set(itertools.chain.from_iterable([s['data'] for s in args.stages]))
+    all_dfs_dict = get_features_df(videos_df, unique_data)
 
     #print(features_df)
     #print(features_df.iloc[0])
@@ -308,10 +319,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='cluster videos from a youtube playlist based on the available data')
     parser.add_argument('-v','--version',help='display version', action='store_true')
     parser.add_argument('--file', help='the filename containing the playlist video ids', type=str, default='WL.csv')
-    parser.add_argument('--array',help='use array data columns', action='store_true', default=False)
-    parser.add_argument('--numerical',help='use numerical data columns', action='store_true', default=False)
-    parser.add_argument('--categorical',help='use categorical data columns', type=int, choices=[0,1,2], default=0)
-    parser.add_argument('--text',help='use text data columns', action='store_true', default=False)
     parser.add_argument('--pca',help='compress the features dataframe using pca', action='store_true', default=False)
     parser.add_argument('--pca_variance',help='if using pca, how much variance to retain',type=float,default=0.95)
     parser.add_argument('--display',help='how to display the scatter plot: 2d/3d', choices=[2,3], type=int, default=3)
@@ -320,11 +327,17 @@ if __name__ == "__main__":
     parser.add_argument('--max_clusters',help='maximum number of clusters',type=int,default=10)
     parser.add_argument('--scorer',help='the scorer to use for choosing the best cluster',type=str,default='silhouette_score',choices=['silhouette_score','inertia','calinski_harabasz_score','davies_bouldin_score'])
     parser.add_argument('--scorers',help='which scorers to calculate',type=str,default='silhouette_score,inertia,calinski_harabasz_score,davies_bouldin_score')
+    parser.add_argument('--stages',help='stages of clustering',type=str,default='best_K_means:array,text|best_K_means:categorical_1')
  
     args = parser.parse_args()
     args.scorers = args.scorers.split(',')
     if args.scorer not in args.scorers:
         raise ValueError('the selected scorer {} is not present in the scorers list {}'.format(args.scorer, args.scorers))
+    
+    stages = args.stages.split('|')
+    stages = [s.split(':') for s in stages]
+    stages = [{'method':s[0], 'data':s[1].split(',')} for s in stages]
+    args.stages = stages
 
     if args.version:
         print('1.0.0')
