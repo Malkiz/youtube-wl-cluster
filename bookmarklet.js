d11f8447b6c15a66ca079196aa3984c8a6166ec2
malkiz <omieyal@gmail.com>
2020-07-26 18:44:09 +0300

    exec

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,8 +1,10 @@
 javascript:(function() {
 	fetch('https://raw.githubusercontent.com/Malkiz/youtube-wl-cluster/master/youtube_sort.js', { cache: 'reload' })
 	.then(res => res.text())
-	.then(script => eval(script))
-	.then(() => youtube_sort_malkiz({client_id: '', api_key: ''}))
+	.then(script => {
+		eval(script)
+		return youtube_sort_malkiz({client_id: '', api_key: ''})
+	})
 	.catch(e => {
 		console.error(e);
 		document.write(`Something went wrong, please refer to <a href='https://github.com/Malkiz/youtube-wl-cluster'>script homepage</a> for the latest updates.`);
